b17ae5897a149804b1e0b7c0284556d1dbcdd36b
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-01-26 23:56:07 -0600

    Implemented random scanning of BLE devices

--- bluetoothlab/ViewController.swift
+++ bluetoothlab/ViewController.swift
@@ -7,22 +7,25 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class ViewController: UIViewController, UITextFieldDelegate {
 
 
     @IBOutlet weak var inputTextField: UITextField!
-    
+    var centralManager: CBCentralManager!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         inputTextField.delegate = self;
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
     //Hide keyboard when the user touches outside keyboard
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -36,5 +39,29 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
 
+}
+
+extension ViewController: CBCentralManagerDelegate{
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+        case .unknown:
+            print("central.state is .unknown")
+        case .resetting:
+            print("central.state is .resetting")
+        case .unsupported:
+            print("central.state is .unsupported")
+        case .unauthorized:
+            print("central.state is .unauthorized")
+        case .poweredOff:
+            print("central.state is .poweredOff")
+        case .poweredOn:
+            print("central.state is .poweredOn")
+            centralManager.scanForPeripherals(withServices: nil)
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        print(peripheral)
+    }
 }
 
