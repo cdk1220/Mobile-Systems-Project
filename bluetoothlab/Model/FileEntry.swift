8597cbbe573faa20f74a040a933cdcd65095d322
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-03-17 17:49:24 -0500

    Created a structure to model a file

---
+++ bluetoothlab/Model/FileEntry.swift
@@ -0,0 +1,14 @@
+//
+//  FileEntry.swift
+//  bluetoothlab
+//
+//  Created by Don Chalanga Kuruppu on 3/17/18.
+//  Copyright © 2018 Don Chalanga Kuruppu. All rights reserved.
+//
+
+import Foundation
+
+struct File {
+    public var name: String
+    public var availability: String
+}
