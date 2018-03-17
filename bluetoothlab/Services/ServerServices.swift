2237eebcfd9854770f2900100fb5c47e7c0df35c
Don Chalanga Kuruppu <donchalangakuruppu@Dons-iMac.local>
2018-03-17 17:56:23 -0500

    Created a class to implement file services such as creating, deleting files for the file server part.

---
+++ bluetoothlab/Services/ServerServices.swift
@@ -0,0 +1,13 @@
+//
+//  ServerServices.swift
+//  bluetoothlab
+//
+//  Created by Don Chalanga Kuruppu on 3/17/18.
+//  Copyright © 2018 Don Chalanga Kuruppu. All rights reserved.
+//
+
+import Foundation
+
+class ServerServices {
+    static let serverServicesInstance = ServerServices()
+}
